e4a446996c5ec6d60dc452626ae645cf66d3566f
Alex <delete.rap.stupid@gmail.com>
2024-11-30 16:55:32 +0100

    add worker model

--- cleaning/models.py
+++ cleaning/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.db.models import ForeignKey, CASCADE, CharField, ManyToManyField, SET_NULL, DateTimeField, TextField, \
-    BooleanField
 
 
 class Train(models.Model):
@@ -11,12 +9,12 @@ class Train(models.Model):
         APPROVED = "approved", "Cleaning is approved"
         CANCELED = "canceled", "Cleaning is canceled"
 
-    cleaning_type = ForeignKey("CleaningType", on_delete=CASCADE)
-    status = CharField(max_length=31, choices=Status.choices, default=Status.AWAITS)
-    workers = ManyToManyField("Worker", related_name="trains")
-    approval = ForeignKey("Approval", on_delete=SET_NULL, null=True)
-    start_time = DateTimeField()
-    end_time = DateTimeField()
+    cleaning_type = models.ForeignKey("CleaningType", on_delete=models.CASCADE)
+    status = models.CharField(max_length=31, choices=Status.choices, default=Status.AWAITS)
+    workers = models.ManyToManyField("Worker", related_name="trains")
+    approval = models.ForeignKey("Approval", on_delete=models.SET_NULL, null=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
 
 
 class CleaningType(models.Model):
@@ -25,11 +23,11 @@ class CleaningType(models.Model):
         SP1 = "SP1", "SP1"
         SP2 = "SP2", "SP2"
 
-    title = CharField(max_length=3, choices=Title.choices, default="Null")
-    description = TextField(null=True, blank=True)
+    title = models.CharField(max_length=3, choices=Title.choices, default="Null")
+    description = models.TextField(null=True, blank=True)
 
 
 class Approval(models.Model):
-    worker = ForeignKey("Worker", on_delete=CASCADE, related_name="approvals")
-    status = BooleanField()
-    comments = TextField(null=True, blank=True)
+    worker = models.ForeignKey("Worker", on_delete=models.CASCADE, related_name="approvals")
+    status = models.BooleanField()
+    comments = models.TextField(null=True, blank=True)
