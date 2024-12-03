5835055f818636bfa25cb42f1b3eef8c739838b4
Alex <delete.rap.stupid@gmail.com>
2024-12-03 16:50:51 +0100

    added more logic to model Train

--- cleaning/urls.py
+++ cleaning/urls.py
@@ -5,13 +5,22 @@ from cleaning.views import (
     TrainListView,
     stamp_date_start,
     stamp_date_end,
+    TrainCreateView,
+    TrainUpdateView,
+    TrainDeleteView, approval_create_view, TrainDetailView,
 )
 
 urlpatterns = [
     path("", index, name="index"),
     path("trains/", TrainListView.as_view(), name="trains-list"),
-    path("trains/<int:pk>/start_time/", stamp_date_start, name="clean-time-start"),
-    path("trains/<int:pk>/end_time/", stamp_date_end, name="clean-time-end"),
+    path("trains/<int:pk>/start-time/", stamp_date_start, name="clean-time-start"),
+    path("trains/<int:pk>/end-time/", stamp_date_end, name="clean-time-end"),
+    path("trains/create/", TrainCreateView.as_view(), name="create-train"),
+    path("trains/<int:pk>/update/", TrainUpdateView.as_view(), name="update-train"),
+    path("trains/<int:pk>/delete/", TrainDeleteView.as_view(), name="delete-train"),
+    path("trains/<int:pk>/detail/", TrainDetailView.as_view(), name="detail-train"),
+    path("trains/<int:pk>/approval-create/", approval_create_view, name="approval-create")
+
 ]
 
 
--- cleaning/views.py
+++ cleaning/views.py
@@ -1,11 +1,13 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.utils.timezone import now
 from django.views import generic
 
+from cleaning.forms import TrainCreateForm, ApprovalForm, TrainSearchForm
 from cleaning.models import Train
 
 def index(request):
@@ -35,15 +37,84 @@ class TrainListView(LoginRequiredMixin, generic.ListView):
     context_object_name = "trains_list"
     template_name = "cleaning/trains_list.html"
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TrainListView, self).get_context_data(**kwargs)
+        search_name = self.request.GET.get("name", "")
+        context["search_form"] = TrainSearchForm(
+            initial={"name": search_name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Train.objects.prefetch_related("workers")
+        search_name = self.request.GET.get("name")
+        if search_name:
+            return queryset.filter(name__icontains=search_name)
+        return queryset
+
 
 def stamp_date_start(request, pk):
     train = Train.objects.get(id=pk)
     train.start_time = now()
+    train.status = Train.Status.IN_PROGRESS
     train.save()
     return HttpResponseRedirect(reverse_lazy("cleaning:trains-list"))
 
 def stamp_date_end(request, pk):
     train = Train.objects.get(id=pk)
     train.end_time = now()
+    train.status = Train.Status.COMPLETED
     train.save()
     return HttpResponseRedirect(reverse_lazy("cleaning:trains-list"))
+
+
+class TrainCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Train
+    form_class = TrainCreateForm
+    success_url = reverse_lazy("cleaning:trains-list")
+
+
+class TrainUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Train
+    form_class = TrainCreateForm
+    success_url = reverse_lazy("cleaning:trains-list")
+
+
+class TrainDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Train
+    template_name = "cleaning/confirm_delete_train.html"
+    success_url = reverse_lazy("cleaning:trains-list")
+
+
+class TrainDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Train
+    queryset = Train.objects.all().prefetch_related("approval__worker")
+
+
+
+def approval_create_view(request, pk):
+    train = get_object_or_404(Train, pk=pk)
+
+    if train.approval:
+        messages.info(request, "This train already approved!")
+        return redirect("cleaning:trains-list")
+
+    if request.method == "POST":
+        form = ApprovalForm(request.POST)
+        if form.is_valid():
+            approval = form.save(commit=False)
+            approval.save()
+            train.approval = approval
+            train.save()
+            if approval.status:
+                messages.success(request, "Cleaning approved!")
+                train.status = Train.Status.APPROVED
+            else:
+                messages.success(request, "Cleaning rejected!")
+                train.status = Train.Status.CANCELED
+            train.save()
+            return redirect("cleaning:trains-list")
+    else:
+        form = ApprovalForm(initial={"train": train, "worker": request.user})
+    return render(request, "cleaning/approval_form.html", {'form': form, 'train': train})
+
