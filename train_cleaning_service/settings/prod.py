a7a618e3940a7d342ede14b289f7093bb3f7792e
Alex <delete.rap.stupid@gmail.com>
2024-12-04 19:56:10 +0100

    update minor

--- train_cleaning_service/settings/prod.py
+++ train_cleaning_service/settings/prod.py
@@ -8,6 +8,10 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
